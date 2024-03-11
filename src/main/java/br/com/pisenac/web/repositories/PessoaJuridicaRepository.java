9e6578f9d3758c73ef82d27c301ff451b2a176de
DanielMarcos1 <daniel04pessoal@gmail.com>
2024-03-11 16:31:14 -0300

    Creating new services such as AlunoService, ProfessorService, ResultadoNotaService, MensalidadeService, EmprestimoService, PessoaFisicaService

--- src/main/java/br/com/pisenac/web/repositories/PessoaJuridicaRepository.java
+++ src/main/java/br/com/pisenac/web/repositories/PessoaJuridicaRepository.java
@@ -1,6 +1,6 @@
 package br.com.pisenac.web.repositories;
 
-import br.com.pisenac.web.entities.empresa.PessoaJuridica;
+import br.com.pisenac.web.entities.PessoaJuridica;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
